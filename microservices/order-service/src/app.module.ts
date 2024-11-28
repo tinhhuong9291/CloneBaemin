34d6e50fe73c31447d060beb930a64d74be05a12
tinhhuong9291 <tien3186.tn@gmail.com>
2024-11-28 21:27:48 +0700

    feat user : update schema,docker file

--- microservices/order-service/src/app.module.ts
+++ microservices/order-service/src/app.module.ts
@@ -3,12 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from './database/database.module';
+import { Order, OrderSchema } from './schemas/order.schema';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     DatabaseModule,
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        // Check this line
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: './.env',
+    }),
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
     ClientsModule.register([
       {
         name: 'ORDER_NAME',
---
+++ microservices/order-service/src/schemas/order.schema.ts
@@ -0,0 +1,61 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { AbstractDocument } from 'src/database/abstract.schema';
+
+@Schema({ versionKey: false })
+export class Order extends AbstractDocument {
+  @Prop({ required: true, unique: true })
+  order_id: number;
+
+  @Prop({ required: true })
+  user_id: string;
+
+  @Prop({ required: true })
+  address_id: number;
+
+  @Prop({ required: true })
+  store_id: number;
+
+  @Prop()
+  voucher_id?: number;
+
+  @Prop()
+  method_id?: number;
+
+  @Prop({ maxlength: 255 })
+  message?: string;
+
+  @Prop()
+  total_discount?: number;
+
+  @Prop({ required: true })
+  total_price: number;
+
+  @Prop({ maxlength: 50 })
+  status?: string;
+
+  @Prop()
+  service_fee?: number;
+
+  @Prop()
+  shipping_price?: number;
+
+  @Prop({ default: false })
+  is_removed: boolean;
+
+  @Prop({ default: Date.now })
+  created_at: Date;
+
+  @Prop({ default: Date.now })
+  updated_at: Date;
+
+  @Prop({ type: [String] })
+  order_foods?: string[];
+
+  @Prop({ type: [String] })
+  payments?: string[];
+
+  @Prop({ type: [String] })
+  order_food_options?: string[];
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
