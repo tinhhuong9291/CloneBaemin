5f674c3da8bbdc2ab236fe7984e64d95f465037f
tinhhuong9291 <tien3186.tn@gmail.com>
2024-11-28 12:33:27 +0700

    feat food add dockerfile

--- microservices/user-service/src/app.module.ts
+++ microservices/user-service/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    ClientsModule.register([
+      {
+        name: 'USER_NAME',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:1234@localhost:5672'],
+          queue: 'user_queue',
+          queueOptions: {
+            durable: true,
+          },
+          persistent: true,
+        },
+      },
+    ]),
+    // RedisCacheModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
